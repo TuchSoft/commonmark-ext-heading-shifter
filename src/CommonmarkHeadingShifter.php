d919841c4b13be107cfcda08757d78faf0a1191b
Mattia Bonzi <mattia@mattiabonzi.it>
2025-07-14 16:17:15 +0200

    Create CommonmarkHeadingShifter.php

---
+++ src/CommonmarkHeadingShifter.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace TuchSoft\CommonmarkHeadingShifter;
+
+use League\CommonMark\Node\Node;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Node\Query;
+use League\CommonMark\Event\DocumentParsedEvent;
+
+
+class CommonmarkHeadingShifter
+{
+    private int $shiftBy;
+
+    public function __construct(int $shiftBy = 1)
+    {
+        $this->shiftBy = $shiftBy;
+    }
+
+
+    public function onDocumentParsed(DocumentParsedEvent $e): void
+    {
+        $document = $e->getDocument();
+        $query = (new Query())->where( function (Node $node): bool {
+                return $node instanceof Heading;
+            });
+        
+        foreach ($query->findAll($document) as $node) {
+                $newLevel = $node->getLevel() + $this->shiftBy;
+                $node->setLevel(max(1,  min(6, $newLevel)));    
+        }
+    }
+
+
+}
